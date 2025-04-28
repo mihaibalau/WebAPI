117a94ef483e4cb5756941323702461a7a78025a
Robert076 <77006934+Robert076@users.noreply.github.com>
2025-04-28 19:51:36 +0300

    department model

---
+++ Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    public class Department
+    {
+        [Key]  // Primary Key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  // Auto-increment (IDENTITY(1,1))
+        public int DepartmentId { get; set; }
+
+        [Required]  // NOT NULL
+        [MaxLength(100)]  // Limit the name length to 100 characters
+        public string Name { get; set; }
+    }
+}
