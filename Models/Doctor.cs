7b95184c9994a6a77170b52212634cb228b21b91
Robert076 <77006934+Robert076@users.noreply.github.com>
2025-04-29 20:35:37 +0300

    doctor model

---
+++ Models/Doctor.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    public class Doctor
+    {
+        [Key]
+        [ForeignKey("User")]
+        public int UserId { get; set; }  // Primary Key and FK to Users
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [Range(0.0, 5.0)]
+        public double DoctorRating { get; set; } = 0.0;
+
+        [Required]
+        [MaxLength(50)]
+        public string LicenseNumber { get; set; }
+
+        public string? CareerInfo { get; set; }
+
+        // Navigation properties
+        public virtual User User { get; set; }
+
+        public virtual Department Department { get; set; }
+    }
+}
