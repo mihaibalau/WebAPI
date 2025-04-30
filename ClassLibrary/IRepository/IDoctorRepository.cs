11e96ab65abb0c499ff260f79b1f10de9b8c1fae
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-05-01 00:07:26 +0300

    Create the interface for the doctor's repo

---
+++ ClassLibrary/IRepository/IDoctorRepository.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.IRepository
+{
+   public interface IDoctorRepository
+    {
+        /// <summary>
+        /// Gets all doctors.
+        /// </summary>
+        /// <returns>A list of doctors</returns>
+        Task<List<Doctor>> GetAllDoctorsAsync();
+
+        /// <summary>
+        /// Gets a doctor by its unique identifier.
+        /// </summary>
+        /// <param name="id">The id of the user</param>
+        /// <returns>The doctor with the given user id.</returns>
+        Task<Doctor> GetDoctorByUserIdAsync(int id);
+
+        /// <summary>
+        /// Gets a list of doctors by department id.
+        /// </summary>
+        /// <param name="departmentId">The id of the departemnt</param>
+        /// <returns>The list of doctors with the given department id</returns>
+        Task<List<Doctor>> GetDoctorsByDepartmentIdAsync(int departmentId);
+
+        Task AddDoctorAsync(Doctor doctor);
+
+        Task DeleteDoctorAsync(int id);
+    }
+}
