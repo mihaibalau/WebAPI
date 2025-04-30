00c3b6bad4b3ff025b14afefb562d5d5fbfcc4e4
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-05-01 00:07:51 +0300

    Create the interface for the user's repo

---
+++ ClassLibrary/IRepository/IUserRepository.cs
@@ -0,0 +1,39 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.IRepository
+{
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Gets all users.
+        /// </summary>
+        /// <returns> A list of users.</returns>
+        Task<List<User>> GetAllUsersAsync();
+
+        /// <summary>
+        /// Gets a user by its unique identifier.
+        /// </summary>
+        /// <param name="id"> the id of the user.</param>
+        /// <returns> The user with the given id.</returns>
+        Task<User> GetUserByIdAsync(int id);
+
+        /// <summary>
+        /// Gets a user by its username.
+        /// </summary>
+        /// <param name="user"> The user to be searched.</param>
+        /// <returns> The user with the given username.</returns>
+        Task AddUserAsync(User user);
+
+        /// <summary>
+        /// Deletes a user by its unique identifier.
+        /// </summary>
+        /// <param name="id"> The id of the user</param>
+        /// <returns> task representing the asynchronous operation.</returns>
+        Task DeleteUserAsync(int id);
+    }
+}
