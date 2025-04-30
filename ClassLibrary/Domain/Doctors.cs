2fab9983c0bb3593655761f763c3876800d6ad59
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-04-30 22:37:52 +0300

    Add Doctors class to Domain namespace

---
+++ ClassLibrary/Domain/Doctors.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain
+{
+    /// <summary>
+    /// Represents a doctor in the system.
+    /// </summary>
+    class Doctors
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the user.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the departement identifier for the doctor.
+        /// </summary>
+        public int DepartmentId { get; set; }
+
+        /// <summary>
+        /// Gets or sets doctor rating for the doctor.
+        /// </summary>
+        public float DoctorRating { get; set; }
+
+        /// <summary>
+        /// Gets or sets the licence number for the doctor.
+        /// </summary>
+        public string LicenceNumber { get; set; }
+    }
+}
