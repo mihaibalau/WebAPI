aff345993ef534eb30cf0d9169958700f052614b
paulb <paulberca@gmail.com>
2025-05-03 02:17:57 +0300

    added auto update functionality

--- WinUI/ViewModel/Implementation/NotificationViewModel.cs
+++ WinUI/ViewModel/Implementation/NotificationViewModel.cs
@@ -8,16 +8,37 @@ using WinUI.ViewModel.Interface;
 
 namespace WinUI.ViewModel.Implementation
 {
-    public class NotificationViewModel : INotificationViewModel
+    public class NotificationViewModel : INotificationViewModel, INotifyPropertyChanged
     {
         private readonly INotificationService notificationService;
 
         public ObservableCollection<Notification> Notifications { get; }
 
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get => errorMessage;
+            private set
+            {
+                if (errorMessage != value)
+                {
+                    errorMessage = value;
+                    OnPropertyChanged(nameof(ErrorMessage));
+                }
+            }
+        }
+
         public NotificationViewModel(INotificationService notificationService)
         {
             this.notificationService = notificationService;
             Notifications = new ObservableCollection<Notification>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
