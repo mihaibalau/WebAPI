7262c63e669a746214586f52b8aba01c7b419433
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-05-02 12:09:47 +0300

    Create the notification  domain

---
+++ ClassLibrary/Domain/Notification.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain
+{
+    /// <summary>
+    /// Represents a notification in the system.
+    /// </summary>
+    public class Notification
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the notification.
+        /// </summary>
+        public int NotificationId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the user associated with the notification.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the notification is scheduled to be delivered.
+        /// </summary>
+        public DateTime DeliveryDateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message content of the notification.
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
