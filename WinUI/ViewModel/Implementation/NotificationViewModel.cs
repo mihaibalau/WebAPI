0db353cba43855461307000722805fd8efa2d29a
paulb <paulberca@gmail.com>
2025-05-03 02:17:01 +0300

    created implementation of notification viewmodel interface

---
+++ WinUI/ViewModel/Implementation/NotificationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using WinUI.Service.NotificationServiceFile;
+using WinUI.ViewModel.Interface;
+
+namespace WinUI.ViewModel.Implementation
+{
+    public class NotificationViewModel : INotificationViewModel
+    {
+        private readonly INotificationService notificationService;
+
+        public ObservableCollection<Notification> Notifications { get; }
+
+        public NotificationViewModel(INotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+            Notifications = new ObservableCollection<Notification>();
+        }
+    }
+}
