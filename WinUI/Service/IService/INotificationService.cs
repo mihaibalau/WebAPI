66683b9251503c584fa8b673b9ade120d6cd31ab
Boar Victor <rotcivraob@yahoo.com>
2025-05-02 18:39:15 +0300

    Notification service and Iservice

---
+++ WinUI/Service/IService/INotificationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace WinUI.Service.IService
+{
+    public interface INotificationService
+    {
+
+        /// <summary>
+        /// Return all notification
+        /// </summary>
+        Task<List<Notification>> GetAllAsync();
+
+        /// <summary>
+        /// Returnează all notification for a user
+        /// </summary>
+        Task<List<Notification>> GetByUserIdAsync(int userId);
+
+        /// <summary>
+        /// Delete notification <paramref name="notificationId"/>, 
+        /// delete notifications only for <paramref name="userId"/>.
+        /// </summary>
+        Task DeleteAsync(int notificationId, int userId);
+
+    }
+}
---
+++ WinUI/Service/NotificationService.cs
@@ -0,0 +1,46 @@
+﻿using ClassLibrary.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.IRepository;
+using WinUI.Service.IService;
+using Domain;
+
+namespace WinUI.Service
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly INotificationRepository repo;
+
+        public NotificationService(INotificationRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public Task<List<Notification>> GetAllAsync()
+        { 
+            return repo.GetAllNotificationsAsync(); 
+        }
+
+        public Task<List<Notification>> GetByUserIdAsync(int userId)
+        { 
+            return repo.GetNotificationsByUserIdAsync(userId); 
+        }
+
+        public async Task DeleteAsync(int notificationId, int userId)
+        {
+            var notification = await repo.GetNotificationByIdAsync(notificationId);
+
+            if (notification == null)
+                throw new KeyNotFoundException($"Notification with ID {notificationId} not found.");
+
+            if (notification.UserId != userId)
+                throw new UnauthorizedAccessException(
+                    $"User {userId} is not allowed to delete notification {notificationId}.");
+
+            await repo.DeleteNotificationAsync(notificationId);
+        }
+    }
+}
