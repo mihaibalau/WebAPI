a5586f7ea8ea788e221360adfd1222c3f60331d7
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-05-04 20:15:55 +0300

    add PatientControllerTest

--- WebApiTests/PatientControllerTest.cs
+++ WebApiTests/PatientControllerTest.cs
@@ -10,86 +10,87 @@ namespace WebApiTests;
 public class PatientControllerTest
 {
     [TestMethod]
-    public async Task GetAllPatients_ReturnsListOfPatients()
+    public async Task GetAllPatients_WithValidController_ReturnsListOfPatients()
     {
         // Arrange
-        var mockRepo = new Mock<IPatientRepository>();
-        var fakePatients = new List<Patient>
+        var _mock_repo = new Mock<IPatientRepository>();
+        var _fake_patients = new List<Patient>
         {
             new Patient { UserId = 1, BloodType = "AB+", EmergencyContact = "Mom", Allergies = "cats, dogs", Weight = 72.6, Height = 167 },
             new Patient { UserId = 2, BloodType = "A-", EmergencyContact = "Dad", Allergies = "cats, dogs", Weight = 92.6, Height = 199 }
         };
-        mockRepo.Setup(repo => repo.GetAllPatientsAsync()).ReturnsAsync(fakePatients);
+        _mock_repo.Setup(_repo => _repo.GetAllPatientsAsync()).ReturnsAsync(_fake_patients);
 
-        var controller = new PatientController(mockRepo.Object);
+        var _controller = new PatientController(_mock_repo.Object);
 
         // Act
-        var result = await controller.GetAllPatients();
-        var okResult = result.Result as OkObjectResult;
+        var _result = await _controller.GetAllPatients();
+        var _ok_result = _result.Result as OkObjectResult;
 
         // Assert
-        Assert.IsNotNull(okResult);
-        var returnedPatients = okResult.Value as List<Patient>;
-        Assert.AreEqual(2, returnedPatients.Count);
+        Assert.IsNotNull(_ok_result);
+        var _returned_patients = _ok_result.Value as List<Patient>;
+        Assert.AreEqual(2, _returned_patients.Count);
     }
 
     [TestMethod]
-    public async Task GetPatientById_ReturnsPatient()
+    public async Task GetPatientById_WithValidPatientId_ReturnsPatient()
     {
         // Arrange
-        var mockRepo = new Mock<IPatientRepository>();
-        var fakePatient = new Patient { UserId = 2, BloodType = "A-", EmergencyContact = "Dad", Allergies = "cats, dogs", Weight = 92.6, Height = 199 };
+        var _patient_id = 2;
+        var _mock_repo = new Mock<IPatientRepository>();
+        var _fake_patient = new Patient { UserId = 2, BloodType = "A-", EmergencyContact = "Dad", Allergies = "cats, dogs", Weight = 92.6, Height = 199 };
 
-        mockRepo.Setup(repo => repo.GetPatientByUserIdAsync(2)).ReturnsAsync(fakePatient);
+        _mock_repo.Setup(_repo => _repo.GetPatientByUserIdAsync(_patient_id)).ReturnsAsync(_fake_patient);
 
-        var controller = new PatientController(mockRepo.Object);
+        var _controller = new PatientController(_mock_repo.Object);
 
         // Act
-        var result = await controller.GetPatientById(2);
-        var okResult = result.Result as OkObjectResult;
+        var _result = await _controller.GetPatientById(_patient_id);
+        var _ok_result = _result.Result as OkObjectResult;
 
         // Assert
-        Assert.IsNotNull(okResult);
-        var returnedPatient = okResult.Value as Patient;
-        Assert.AreEqual(2, returnedPatient.UserId);
+        Assert.IsNotNull(_ok_result);
+        var _returned_patient = _ok_result.Value as Patient;
+        Assert.AreEqual(2, _returned_patient.UserId);
     }
 
     [TestMethod]
-    public async Task CreatePatient_ReturnsCreatedAtAction()
+    public async Task CreatePatient_WithValidPatient_ReturnsCreatedAtAction()
     {
         // Arrange
-        var mockRepo = new Mock<IPatientRepository>();
-        var fakePatient = new Patient { UserId = 2, BloodType = "A-", EmergencyContact = "Dad", Allergies = "cats, dogs", Weight = 92.6, Height = 199 };
-        mockRepo.Setup(repo => repo.AddPatientAsync(fakePatient)).Returns(Task.CompletedTask);
+        var _mock_repo = new Mock<IPatientRepository>();
+        var _fake_patient = new Patient { UserId = 2, BloodType = "A-", EmergencyContact = "Dad", Allergies = "cats, dogs", Weight = 92.6, Height = 199 };
+        _mock_repo.Setup(repo => repo.AddPatientAsync(_fake_patient)).Returns(Task.CompletedTask);
 
-        var controller = new PatientController(mockRepo.Object);
+        var _controller = new PatientController(_mock_repo.Object);
 
         // Act
-        var result = await controller.CreatePatient(fakePatient);
-        var createdAt = result as CreatedAtActionResult;
+        var _result = await _controller.CreatePatient(_fake_patient);
+        var _created_at = _result as CreatedAtActionResult;
 
         // Assert
-        Assert.IsNotNull(createdAt);
-        Assert.AreEqual(fakePatient.UserId, ((Patient)createdAt.Value).UserId);
+        Assert.IsNotNull(_created_at);
+        Assert.AreEqual(_fake_patient.UserId, ((Patient)_created_at.Value).UserId);
     }
 
     [TestMethod]
-    public async Task DeletePatient_ReturnsNoContent()
+    public async Task DeletePatient_WithValidPatientId_ReturnsNoContent()
     {
         // Arrange
-        var mockRepo = new Mock<IPatientRepository>();
-        int PatientId = 1;
+        var _mock_repo = new Mock<IPatientRepository>();
+        int _patient_id = 1;
 
         // No setup needed if the method succeeds (completes without exception)
-        mockRepo.Setup(repo => repo.DeletePatientAsync(PatientId)).Returns(Task.CompletedTask);
+        _mock_repo.Setup(_repo => _repo.DeletePatientAsync(_patient_id)).Returns(Task.CompletedTask);
 
-        var controller = new PatientController(mockRepo.Object);
+        var _controller = new PatientController(_mock_repo.Object);
 
         // Act
-        var result = await controller.DeletePatient(PatientId);
+        var _result = await _controller.DeletePatient(_patient_id);
 
         // Assert
-        Assert.IsInstanceOfType(result, typeof(NoContentResult));
-        mockRepo.Verify(repo => repo.DeletePatientAsync(PatientId), Times.Once);
+        Assert.IsInstanceOfType(_result, typeof(NoContentResult));
+        _mock_repo.Verify(_repo => _repo.DeletePatientAsync(_patient_id), Times.Once);
     }
 }
