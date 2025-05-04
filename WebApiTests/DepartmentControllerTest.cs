c8e7b21a03348e0c954e3d42c4eddb1a315de6d1
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-05-04 19:45:41 +0300

    update DepartmentControllerTest

--- WebApiTests/DepartmentControllerTest.cs
+++ WebApiTests/DepartmentControllerTest.cs
@@ -13,66 +13,66 @@ namespace WebApiTests
     public class DepartmentControllerTest
     {
         [TestMethod]
-        public async Task GetAllDoctors_ReturnsListOfDepartments()
+        public async Task GetAllDepartments_WiithValidDoctorDepartments_ReturnsListOfDepartments()
         {
             // Arrange
-            var mockRepo = new Mock<IDepartmentRepository>();
-            var fakeDepartments = new List<Department>
+            var _mock_repo = new Mock<IDepartmentRepository>();
+            var _fake_departments = new List<Department>
             {
                 new Department { Id = 1, Name = "John" },
                 new Department { Id = 2, Name = "Johnny" },
             };
-            mockRepo.Setup(repo => repo.GetAllDepartmentsAsync()).ReturnsAsync(fakeDepartments);
+            _mock_repo.Setup(_repo => _repo.GetAllDepartmentsAsync()).ReturnsAsync(_fake_departments);
 
-            var controller = new DepartmentController(mockRepo.Object);
+            var _controller = new DepartmentController(_mock_repo.Object);
 
             // Act
-            var result = await controller.GetAllDepartments();
-            var okResult = result.Result as OkObjectResult;
+            var _result = await _controller.GetAllDepartments();
+            var _ok_result = _result.Result as OkObjectResult;
 
             // Assert
-            Assert.IsNotNull(okResult);
-            var returnedDepartments = okResult.Value as List<Department>;
+            Assert.IsNotNull(_ok_result);
+            var returnedDepartments = _ok_result.Value as List<Department>;
             Assert.AreEqual(2, returnedDepartments.Count);
         }
 
         [TestMethod]
-        public async Task CreateDepartment_ReturnsCreatedAtAction()
+        public async Task CreateDepartment_WithValidDepartmentInput_ReturnsCreatedAtActionResult()
         {
             // Arrange
-            var mockRepo = new Mock<IDepartmentRepository>();
-            var fakeDepartment = new Department { Id = 1, Name = "John" };
-            mockRepo.Setup(repo => repo.AddDepartmentAsync(fakeDepartment)).Returns(Task.CompletedTask);
+            var _mock_repo = new Mock<IDepartmentRepository>();
+            var _fake_department = new Department { Id = 1, Name = "John" };
+            _mock_repo.Setup(_repo => _repo.AddDepartmentAsync(_fake_department)).Returns(Task.CompletedTask);
 
-            var controller = new DepartmentController(mockRepo.Object);
+            var _controller = new DepartmentController(_mock_repo.Object);
 
             // Act
-            var result = await controller.CreateDepartment(fakeDepartment);
-            var createdAt = result as CreatedAtActionResult;
+            var _result = await _controller.CreateDepartment(_fake_department);
+            var _created_at = _result as CreatedAtActionResult;
 
             // Assert
-            Assert.IsNotNull(createdAt);
-            Assert.AreEqual(fakeDepartment.Id, ((Department)createdAt.Value).Id);
+            Assert.IsNotNull(_created_at);
+            Assert.AreEqual(_fake_department.Id, ((Department)_created_at.Value).Id);
         }
 
         [TestMethod]
-        public async Task DeleteDepartment_ReturnsNoContent()
+        public async Task DeleteDepartment_WithValidDoctorID_ReturnsNoContent()
         {
             // Arrange
-            var mockRepo = new Mock<IDepartmentRepository>();
-            int doctorId = 1;
+            var _mock_repo = new Mock<IDepartmentRepository>();
+            int _doctor_id = 1;
 
             // No setup needed if the method succeeds (completes without exception)
-            mockRepo.Setup(repo => repo.DeleteDepartmentAsync(doctorId)).Returns(Task.CompletedTask);
+            _mock_repo.Setup(_repo => _repo.DeleteDepartmentAsync(_doctor_id)).Returns(Task.CompletedTask);
 
-            var controller = new DepartmentController(mockRepo.Object);
+            var _controller = new DepartmentController(_mock_repo.Object);
 
             // Act
-            var result = await controller.DeleteDepartment(doctorId);
+            var _result = await _controller.DeleteDepartment(_doctor_id);
 
             // Assert
-            Assert.IsInstanceOfType(result, typeof(NoContentResult));
-            mockRepo.Verify(repo => repo.DeleteDepartmentAsync(doctorId), Times.Once);
+            Assert.IsInstanceOfType(_result, typeof(NoContentResult));
+            _mock_repo.Verify(repo => repo.DeleteDepartmentAsync(_doctor_id), Times.Once);
         }
     }
 }
