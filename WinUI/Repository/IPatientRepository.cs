2631d349c82d1ab559f1a926cf0be518d8fc4ecf
Mihai Balau <balaumihai99@gmail.com>
2025-05-05 03:33:14 +0300

    Coding Style Improvements & Checking #3
    Checked + Small Improvements
    WinUI:
    - Repository

--- WinUI/Repository/IPatientRepository.cs
+++ WinUI/Repository/IPatientRepository.cs
@@ -9,18 +9,105 @@ namespace WinUI.Repository
 {
     public interface IPatientRepository
     {
+        /// <summary>
+        /// Retrieves a list of all patients with their associated details.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of <see cref="PatientJointModel"/> objects.</returns>
         Task<List<PatientJointModel>> getAllPatients();
-        Task<PatientJointModel> getPatientByUserId(int userId);
-        Task<bool> updatePassword(int userId, string password);
-        Task<bool> updateEmail(int userId, string email);
-        Task<bool> updateUsername(int userId, string username);
-        Task<bool> updateName(int userId, string name);
-        Task<bool> updateBirthDate(int userId, DateOnly birthDate);
-        Task<bool> updateAddress(int userId, string address);
-        Task<bool> updatePhoneNumber(int userId, string phoneNumber);
-        Task<bool> updateEmergencyContact(int userId, string emergencyContact);
-        Task<bool> updateWeight(int userId, double weight);
-        Task<bool> updateHeight(int userId, int height);
-        Task<bool> logUpdate(int userId, ActionType type);
+
+        /// <summary>
+        /// Retrieves a patient's details by their user ID.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the <see cref="PatientJointModel"/> for the specified user.</returns>
+        Task<PatientJointModel> getPatientByUserId(int user_id);
+
+        /// <summary>
+        /// Updates the password for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="password">The new password to set.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updatePassword(int user_id, string password);
+
+        /// <summary>
+        /// Updates the email address for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="email">The new email address to set.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updateEmail(int user_id, string email);
+
+        /// <summary>
+        /// Updates the username for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="username">The new username to set.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updateUsername(int user_id, string username);
+
+        /// <summary>
+        /// Updates the full name for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="name">The new name to set.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updateName(int user_id, string name);
+
+        /// <summary>
+        /// Updates the birth date for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="birth_date">The new birth date to set.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updateBirthDate(int user_id, DateOnly birth_date);
+
+        /// <summary>
+        /// Updates the address for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="address">The new address to set.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updateAddress(int user_id, string address);
+
+        /// <summary>
+        /// Updates the phone number for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="phone_number">The new phone number to set.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updatePhoneNumber(int user_id, string phone_number);
+
+        /// <summary>
+        /// Updates the emergency contact information for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="emergency_contact">The new emergency contact to set.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updateEmergencyContact(int user_id, string emergency_contact);
+
+        /// <summary>
+        /// Updates the weight for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="weight">The new weight to set (in kilograms).</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updateWeight(int user_id, double weight);
+
+        /// <summary>
+        /// Updates the height for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="height">The new height to set (in centimeters).</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful; otherwise, false.</returns>
+        Task<bool> updateHeight(int user_id, int height);
+
+        /// <summary>
+        /// Logs an update action for a specified user.
+        /// </summary>
+        /// <param name="user_id">The unique identifier of the user.</param>
+        /// <param name="type">The type of action performed.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is true if the log was successful; otherwise, false.</returns>
+        Task<bool> logUpdate(int user_id, ActionType type);
     }
 }
