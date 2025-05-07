38ee734a559f95df4c5d7a729397ec7249a8ad0a
delight <baranorbi12@gmail.com>
2025-05-07 21:28:01 +0300

    fix comments

--- WinUI/Repository/IPatientRepository.cs
+++ WinUI/Repository/IPatientRepository.cs
@@ -10,19 +10,6 @@ namespace WinUI.Repository
 {
     public interface IPatientRepository
     {
-        /*Task<List<PatientJointModel>> getAllPatients();
-        Task<PatientJointModel> getPatientByUserId(int userId);
-        Task<bool> updatePassword(int userId, string password);
-        Task<bool> updateEmail(int userId, string email);
-        Task<bool> updateUsername(int userId, string username);
-        Task<bool> updateName(int userId, string name);
-        Task<bool> updateBirthDate(int userId, DateOnly birthDate);
-        Task<bool> updateAddress(int userId, string address);
-        Task<bool> updatePhoneNumber(int userId, string phoneNumber);
-        Task<bool> updateEmergencyContact(int userId, string emergencyContact);
-        Task<bool> updateWeight(int userId, double weight);
-        Task<bool> updateHeight(int userId, int height);
-        Task<bool> logUpdate(int userId, ActionType type);*/
         /// <summary>
         /// Get all patients from the database
         /// </summary>
