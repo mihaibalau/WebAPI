f5dd249cb78ba18dd6453d28b1ad42cac8b2fad2
Robert076 <77006934+Robert076@users.noreply.github.com>
2025-05-10 10:25:23 +0300

    notification service is good

--- WinUI/ViewModel/NotificationViewModel.cs
+++ WinUI/ViewModel/NotificationViewModel.cs
@@ -11,39 +11,38 @@ namespace WinUI.ViewModel
     {
         private readonly INotificationService _notification_service;
         public int _user_id { get; set; }
-
         public ObservableCollection<Notification> _notifications_collection { get; } = new();
 
-        public NotificationViewModel(NotificationService _notification_service, int _user_id)
+        public NotificationViewModel(NotificationService notification_service, int user_id)
         {
-            this._notification_service = _notification_service;
-            this._user_id = _user_id;
+            this._notification_service = notification_service;
+            this._user_id = user_id;
         }
 
-        public async Task loadNotificationsAsync(int _user_id)
+        public async Task loadNotificationsAsync(int user_id)
         {
-            List<Notification> _notifications = await this._notification_service.getNotificationsByUserIdAsync(_user_id);
+            List<Notification> notifications = await this._notification_service.getNotificationsByUserIdAsync(user_id);
 
-            _notifications_collection.Clear();
+            this._notifications_collection.Clear();
 
-            foreach (Notification _notification in _notifications)
-                _notifications_collection.Add(_notification);
+            foreach (Notification notification in notifications)
+                this._notifications_collection.Add(notification);
         }
 
-        public async Task deleteNotificationAsync(int _notification_id, int _user_id)
+        public async Task deleteNotificationAsync(int notification_id, int user_id)
         {
-                await this._notification_service.deleteNotificationAsync(_notification_id, _user_id);
-                Notification _notification = this.findNotificationById(_notification_id);
-                if (_notification != null)
-                    _notifications_collection.Remove(_notification);
+                await this._notification_service.deleteNotificationAsync(notification_id, user_id);
+                Notification notification = this.findNotificationById(notification_id);
+                if (notification != null)
+                    this._notifications_collection.Remove(notification);
         }
 
-        public Notification findNotificationById(int _id)
+        public Notification findNotificationById(int id)
         {
-            foreach (Notification _notification in _notifications_collection)
+            foreach (Notification notification in this._notifications_collection)
             {
-                if (_notification._notification_id == _id)
-                    return _notification;
+                if (notification._notification_id == id)
+                    return notification;
             }
             return null;
         }
