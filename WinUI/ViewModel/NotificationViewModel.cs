00535d0bf02719b21f4789680df7fa8377619673
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-05-10 20:37:01 +0300

    Refactor some variables names in the notification view model

--- WinUI/ViewModel/NotificationViewModel.cs
+++ WinUI/ViewModel/NotificationViewModel.cs
@@ -10,23 +10,23 @@ namespace WinUI.ViewModel
     public class NotificationViewModel
     {
         private readonly INotificationService _notification_service;
-        public int _user_id { get; set; }
-        public ObservableCollection<Notification> _notifications_collection { get; } = new();
+        public int userId { get; set; }
+        public ObservableCollection<Notification> notificationsCollection { get; } = new();
 
         public NotificationViewModel(NotificationService notification_service, int user_id)
         {
             this._notification_service = notification_service;
-            this._user_id = user_id;
+            this.userId = user_id;
         }
 
         public async Task loadNotificationsAsync(int user_id)
         {
             List<Notification> notifications = await this._notification_service.getNotificationsByUserIdAsync(user_id);
 
-            this._notifications_collection.Clear();
+            this.notificationsCollection.Clear();
 
             foreach (Notification notification in notifications)
-                this._notifications_collection.Add(notification);
+                this.notificationsCollection.Add(notification);
         }
 
         public async Task deleteNotificationAsync(int notification_id, int user_id)
@@ -34,14 +34,14 @@ namespace WinUI.ViewModel
                 await this._notification_service.deleteNotificationAsync(notification_id, user_id);
                 Notification notification = this.findNotificationById(notification_id);
                 if (notification != null)
-                    this._notifications_collection.Remove(notification);
+                    this.notificationsCollection.Remove(notification);
         }
 
         public Notification findNotificationById(int id)
         {
-            foreach (Notification notification in this._notifications_collection)
+            foreach (Notification notification in this.notificationsCollection)
             {
-                if (notification._notification_id == id)
+                if (notification.notificationId == id)
                     return notification;
             }
             return null; 
