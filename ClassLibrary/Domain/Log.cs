bdab491d8004de65bb853c706c959f213bf0fb8f
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-05-10 17:05:35 +0300

    Changed Log to use camelCase for getters and setters

--- ClassLibrary/Domain/Log.cs
+++ ClassLibrary/Domain/Log.cs
@@ -8,17 +8,17 @@
         /// <summary>
         /// Gets or sets the unique identifier for the log entry.
         /// </summary>
-        public int log_id { get; set; }
+        public int logId { get; set; }
 
         /// <summary>
         /// Gets or sets the unique identifier for the user associated with the log entry.
         /// </summary>
-        public int user_id { get; set; }
+        public int userId { get; set; }
 
         /// <summary>
         /// Gets or sets the unique identifier for the action type fot the log entry.
         /// </summary>
-        public string action_type{ get; set; }
+        public string actionType{ get; set; }
 
         /// <summary>
         /// Gets or sets the timestamp of the log entry.
