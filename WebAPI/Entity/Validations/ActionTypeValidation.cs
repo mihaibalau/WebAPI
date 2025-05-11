ffe50d771836a7931b8d7919ca367be1c254b726
delight <baranorbi12@gmail.com>
2025-05-11 13:33:05 +0300

    static rename

--- WebAPI/Entity/Validations/ActionTypeValidation.cs
+++ WebAPI/Entity/Validations/ActionTypeValidation.cs
@@ -3,17 +3,17 @@ using System.Linq;
 
 public class ActionTypeValidationAttribute : ValidationAttribute
 {
-    private static readonly string[] _allowed_values = new[]
+    private static readonly string[] s_allowed_values = new[]
     {
         "LOGIN", "LOGOUT", "UPDATE_PROFILE", "CHANGE_PASSWORD", "DELETE_ACCOUNT", "CREATE_ACCOUNT"
     };
 
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        if (value is string str && _allowed_values.Contains(str))
+        if (value is string str && s_allowed_values.Contains(str))
         {
             return ValidationResult.Success;
         }
-        return new ValidationResult($"Invalid ActionType. Allowed values: {string.Join(", ", _allowed_values)}");
+        return new ValidationResult($"Invalid ActionType. Allowed values: {string.Join(", ", s_allowed_values)}");
     }
 }
--- WebAPI/Entity/Validations/BloodTypeValidation.cs
+++ WebAPI/Entity/Validations/BloodTypeValidation.cs
@@ -3,14 +3,14 @@ using System.Linq;
 
 public class BloodTypeValidationAttribute : ValidationAttribute
 {
-    private static readonly string[] _allowed_types =
+    private static readonly string[] s_allowed_types =
         { "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-" };
 
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        if (value is string str && _allowed_types.Contains(str))
+        if (value is string str && s_allowed_types.Contains(str))
             return ValidationResult.Success;
 
-        return new ValidationResult($"Invalid blood type. Allowed values: {string.Join(", ", _allowed_types)}");
+        return new ValidationResult($"Invalid blood type. Allowed values: {string.Join(", ", s_allowed_types)}");
     }
 }
