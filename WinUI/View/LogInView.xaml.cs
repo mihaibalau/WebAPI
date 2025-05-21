ce1c05211290473126cce2bacd62bcd9913b151e
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-05-21 16:22:22 +0300

    fix repo reference

--- WinUI/View/LogInView.xaml.cs
+++ WinUI/View/LogInView.xaml.cs
@@ -44,7 +44,7 @@ namespace WinUI.View
         {
             this.InitializeComponent();
 
-            ILogInRepository _log_in_service = new LogInProxy(new System.Net.Http.HttpClient());
+            Repository.ILogInRepository _log_in_service = new LogInProxy(new System.Net.Http.HttpClient());
             IAuthService _service = new AuthService(_log_in_service);
             this._login_page_view_model = new AuthViewModel(_service);
 
